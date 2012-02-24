6153878f4b741200a75d85c63f0af1089d2b5049
Rob Meijer <rob@polacanthus.net>
2012-02-25 00:08:14 +0100

    Minor patch

--- makedepstuff.sh
+++ makedepstuff.sh
@@ -3,6 +3,6 @@ mkdir jsonme++/usr
 mkdir jsonme++/usr/lib
 mkdir jsonme++/usr/include
 cp  ./json-me.hpp jsonme++/usr/include
-mv ls build/libjsonme.so* jsonme++/usr/lib/
+mv build/libjsonme.so* jsonme++/usr/lib/
 fakeroot dpkg-deb --build jsonme++ jsonme++_`grep Version jsonme++/DEBIAN/control |sed -e 's/.* //'`_amd64.deb
 
